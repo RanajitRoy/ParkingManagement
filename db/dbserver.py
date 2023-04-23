2bdbad264733bfeea214e18064dece60db43bff8
Ranajit Roy <royranajit1@gmail.com>
2023-04-23 15:09:42 -0600

    image upload

--- db/dbserver.py
+++ db/dbserver.py
@@ -1,5 +1,5 @@
 from _dbaccess import _DBAccessObj
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 _dbao = _DBAccessObj(host="172.28.80.1")
@@ -8,7 +8,11 @@ _dbao = _DBAccessObj(host="172.28.80.1")
 @app.get("/is_reg/{name}/{plate_no}")
 async def reg_users(name: str, plate_no : str) -> bool:
     return _dbao.is_registered(name, plate_no)
-    
+
+@app.get("/user/img/")
+async def create_upload_file(file: UploadFile) -> bool:
+    return False
+
 @app.post("/user/{name}/{plate_no}")
 async def reg_users(name: str, plate_no : str) -> bool:
     return _dbao.register_user(name, plate_no)
